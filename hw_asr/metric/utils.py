258eb9392e6fc684e37ebe10cda6b05b42dac7ed
Anastasia Drozdova <avdrozdova3@yandex.ru>
2021-10-15 14:24:30 +0300

    metrics

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -5,6 +5,8 @@ import editdistance
 def calc_cer(target_text, predicted_text) -> float:
     if len(target_text) == 0 and len(predicted_text) != 0:
         return 1.0
+    if len(target_text) == 0:
+        return 0.0
     return editdistance.eval(target_text, predicted_text) / len(target_text)
 
 
@@ -13,6 +15,6 @@ def calc_wer(target_text, predicted_text) -> float:
     predicted_words = predicted_text.split()
     if len(target_words) == 0 and len(predicted_words) != 0:
         return 1.0
-    if len(target_words) == 0 and len(predicted_words) != 0:
-        return 1.0
+    if len(target_words) == 0:
+        return 0.0
     return editdistance.eval(target_words, predicted_words) / len(target_words)
