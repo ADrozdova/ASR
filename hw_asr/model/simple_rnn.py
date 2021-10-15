7c6f50b82ca4bb6bdb86d9f0bad509d6d5d1cabd
Anastasia Drozdova <avdrozdova3@yandex.ru>
2021-10-15 15:19:42 +0300

    model: bidirect lstm

--- hw_asr/model/simple_rnn.py
+++ hw_asr/model/simple_rnn.py
@@ -6,25 +6,25 @@ from hw_asr.base import BaseModel
 
 
 class SimpleRnnModel(BaseModel):
-    def __init__(self, n_feats, n_class, fc_hidden=512, n_layers=2, *args, **kwargs):
+    def __init__(self, n_feats, n_class, fc_hidden=512, n_layers=2, dropout=0.25, *args, **kwargs):
         super().__init__(n_feats, n_class, *args, **kwargs)
         self.n_layers = n_layers
         self.fc_hidden = fc_hidden
-        self.rnn = nn.RNN(n_feats, fc_hidden, n_layers, batch_first=True)
-        self.fc = nn.Linear(fc_hidden, n_class)
+        self.rnn = nn.LSTM(
+            n_feats, fc_hidden, num_layers=n_layers,
+            batch_first=True, dropout=dropout, bidirectional=True
+        )
+
+        self.fc = nn.Linear(2 * fc_hidden, n_class)
         self.layers = Sequential(
             self.rnn,
             self.fc
         )
 
     def forward(self, spectrogram, *args, **kwargs):
-        batch_sz = spectrogram.size(0)
-        hidden = self.init_hidden(batch_sz)
-        output, hidden = self.rnn(spectrogram, hidden)
-        # output = output.contiguous().view(batch_sz, -1, self.fc_hidden)
-        output = output.view(batch_sz, -1, self.fc_hidden)
-        output = self.fc(output)
-        return {"logits": output}
+        output, _ = self.rnn(spectrogram)
+        output = output.view(spectrogram.size(0), -1, 2 * self.fc_hidden)
+        return {"logits": self.fc(output)}
 
     def init_hidden(self, batch_size):
         hidden = zeros(self.n_layers, batch_size, self.fc_hidden)
