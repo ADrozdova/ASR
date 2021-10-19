e1944b1204764b5d6003d2394f3a6e7d6582912d
Anastasia Drozdova <avdrozdova3@yandex.ru>
2021-10-19 23:07:29 +0300

    fixes for quartznet

--- hw_asr/model/quartznet.py
+++ hw_asr/model/quartznet.py
@@ -7,7 +7,7 @@ from hw_asr.base import BaseModel
 
 class ConvBNReLU(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, dilation=1, padding=0, bias=False,
-                 separable=False, activation="relu"):
+                 separable=True, activation="relu"):
         super().__init__()
         if separable:
             layers = [
@@ -33,7 +33,7 @@ class ConvBNReLU(nn.Module):
 
 class QuartzNetBlock(nn.Module):
     def __init__(self, in_channels, out_channels, block_count=3, kernel_size=11, stride=1, residual=True, dilation=1,
-                 activation='relu', separable=False):
+                 activation='relu', separable=True):
         super().__init__()
         padding_val = (dilation * kernel_size) // 2 - 1 if dilation > 1 else kernel_size // 2
         layers = []
@@ -61,9 +61,8 @@ class QuartzNetBlock(nn.Module):
                 kernel_size=1,
                 stride=stride,
                 dilation=dilation,
-                padding=padding_val,
                 separable=False,
-                activation="relu"
+                activation=""
             )
         self.out = nn.ReLU(inplace=True)
         self.net = nn.Sequential(*layers)
@@ -77,19 +76,23 @@ class QuartzNetBlock(nn.Module):
 
 
 class QuartzNet(BaseModel):
-    def __init__(self, n_feats, n_class, n_blocks, repeat, channels, kernel_sz, *args, **kwargs):
+    def __init__(self, n_feats, n_class, n_blocks, repeat, channels, kernel_sz, seq_len, *args, **kwargs):
         super().__init__(n_feats, n_class, *args, **kwargs)
-        layers = [ConvBNReLU(n_feats, channels[0], kernel_sz[0], stride=2)]  # C_1
+        layers = [ConvBNReLU(n_feats, channels[0], kernel_sz[0], stride=2, padding=kernel_sz[0] // 2)]  # C_1
         for i in range(n_blocks):
-            layers.append(QuartzNetBlock(channels[i], channels[i + 1], repeat, kernel_sz[i + 1], separable=True))  # B
-        layers.append(ConvBNReLU(channels[-3], channels[-2], kernel_sz[-3]))  # C_2
+            for j in range(seq_len):
+                layers.append(QuartzNetBlock(channels[i], channels[i + 1], repeat, kernel_sz[i + 1]))  # B
+        layers.append(ConvBNReLU(channels[-3], channels[-2], kernel_sz[-3], dilation=2,
+                                 padding=(2 * kernel_sz[-3]) // 2 - 1))  # C_2
         layers.append(ConvBNReLU(channels[-2], channels[-1], kernel_sz[-2]))  # C_3
-        layers.append(ConvBNReLU(channels[-1], n_class, kernel_sz[-1], dilation=2, bias=True))  # C_4
+        # layers.append(ConvBNReLU(channels[-1], n_class, kernel_sz[-1], dilation=2, bias=True, separable=False))  # C_4
+        layers.append(nn.Conv1d(channels[-1], n_class, kernel_sz[-1], dilation=2, bias=True))  # C_4
         self.net = Sequential(*layers)
 
     def forward(self, spectrogram, *args, **kwargs):
+        spectrogram = transpose(spectrogram, 1, 2)
         output = self.net(spectrogram)
         return {"logits": transpose(output, 1, 2)}
 
     def transform_input_lengths(self, input_lengths):
-        return input_lengths  # we don't reduce time dimension here
+        return input_lengths // 2
