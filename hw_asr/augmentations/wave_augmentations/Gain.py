d79533dd7be81718ee742cc5d32afb6ef56ccde7
Anastasia Drozdova <avdrozdova3@yandex.ru>
2021-10-20 16:15:23 +0300

    fix augs

--- hw_asr/augmentations/wave_augmentations/Gain.py
+++ hw_asr/augmentations/wave_augmentations/Gain.py
@@ -6,8 +6,9 @@ from hw_asr.augmentations.base import AugmentationBase
 
 class Gain(AugmentationBase):
     def __init__(self, *args, **kwargs):
-        self._aug = torch_audiomentations.Gain(*args, **kwargs)
+        self.sample_rate = kwargs.get("sample_rate", 16000)
+        self._aug = torch_audiomentations.Gain(p=kwargs.get("p"))
 
     def __call__(self, data: Tensor):
         x = data.unsqueeze(1)
-        return self._aug(x).squeeze(1)
+        return self._aug(x, self.sample_rate).squeeze(1)
