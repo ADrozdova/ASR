72b6a0412d1fd9f0f0c380dba335df7907fa7f52
Anastasia Drozdova <avdrozdova3@yandex.ru>
2021-10-20 17:04:26 +0300

    add time shift

--- hw_asr/augmentations/wave_augmentations/time_shift.py
+++ hw_asr/augmentations/wave_augmentations/time_shift.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch_audiomentations
 from torch import Tensor
+import torch
 
 from hw_asr.augmentations.base import AugmentationBase
 
@@ -12,8 +13,11 @@ class TimeShift(AugmentationBase):
 
     def __call__(self, data: Tensor):
         start_ = int(np.random.uniform(self.start, self.stop))
+        data = data.squeeze(0)
         if start_ >= 0:
             wav_time_shift = np.r_[data[start_:], np.random.uniform(-0.001, 0.001, start_)]
         else:
             wav_time_shift = np.r_[np.random.uniform(-0.001, 0.001, -start_), data[:start_]]
-        return wav_time_shift
+        if not torch.is_tensor(wav_time_shift):
+            wav_time_shift = torch.from_numpy(wav_time_shift)
+        return wav_time_shift.unsqueeze(0)
