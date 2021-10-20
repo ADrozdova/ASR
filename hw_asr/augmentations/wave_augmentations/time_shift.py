c435f8f9f150b50f73e9d9a95595179fe3805f8e
Anastasia Drozdova <avdrozdova3@yandex.ru>
2021-10-20 18:37:48 +0300

    remove time shift

--- hw_asr/augmentations/wave_augmentations/time_shift.py
+++ hw_asr/augmentations/wave_augmentations/time_shift.py
@@ -18,6 +18,5 @@ class TimeShift(AugmentationBase):
             wav_time_shift = np.r_[data[start_:], np.random.uniform(-0.001, 0.001, start_).astype(np.double)]
         else:
             wav_time_shift = np.r_[np.random.uniform(-0.001, 0.001, -start_).astype(np.double), data[:start_]]
-        if not torch.is_tensor(wav_time_shift):
-            wav_time_shift = torch.from_numpy(wav_time_shift)
+        wav_time_shift = torch.from_numpy(wav_time_shift.astype(np.double))
         return wav_time_shift.unsqueeze(0)
