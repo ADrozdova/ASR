e7b4955e0351940711543ec7e5b52a221a0d321e
Anastasia Drozdova <avdrozdova3@yandex.ru>
2021-10-21 01:12:00 +0300

    ljspeech

--- hw_asr/datasets/__init__.py
+++ hw_asr/datasets/__init__.py
@@ -1,9 +1,11 @@
 from hw_asr.datasets.custom_audio_dataset import CustomAudioDataset
 from hw_asr.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
 from hw_asr.datasets.librispeech_dataset import LibrispeechDataset
+from hw_asr.datasets.lj_speech_dataset import LJSpeechDataset
 
 __all__ = [
     "LibrispeechDataset",
     "CustomDirAudioDataset",
-    "CustomAudioDataset"
+    "CustomAudioDataset",
+    "LJSpeechDataset"
 ]
