998ca30ab8ac9e03054ac22e95ddb2f0db5fcf56
Anastasia Drozdova <avdrozdova3@yandex.ru>
2021-10-22 23:18:17 +0300

    dataset for datasphere

--- hw_asr/datasets/__init__.py
+++ hw_asr/datasets/__init__.py
@@ -2,10 +2,12 @@ from hw_asr.datasets.custom_audio_dataset import CustomAudioDataset
 from hw_asr.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
 from hw_asr.datasets.librispeech_dataset import LibrispeechDataset
 from hw_asr.datasets.lj_speech_dataset import LJSpeechDataset
+from hw_asr.datasets.librispeech_dataset_ds import LibrispeechDatasetDS
 
 __all__ = [
     "LibrispeechDataset",
     "CustomDirAudioDataset",
     "CustomAudioDataset",
-    "LJSpeechDataset"
+    "LJSpeechDataset",
+    "LibrispeechDatasetDS"
 ]
